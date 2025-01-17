e469208af0f150bd0a09be764b4f051ecc661467
Pedro Gómez <pedro@goodnotesapp.com>
2025-01-17 12:02:45 +0100

    Create the app and the main package as different targets

--- Package.swift
+++ Package.swift
@@ -1,12 +1,17 @@
-// swift-tools-version: 5.9.2
+// swift-tools-version: 5.9.1
 import PackageDescription
 
 let package = Package(
     name: "SwiftWASMWorkshop",
+    products: [
+        .library(name: "SwiftWASMWorkshop", targets: ["SwiftWASMWorkshop"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/swiftwasm/carton", from: "1.0.0")
     ],
     targets: [
-        .executableTarget(name: "SwiftWASMWorkshop")
+        .executableTarget(name: "SwiftWASMWorkshopApp"),
+        .target(name: "SwiftWASMWorkshop", dependencies: []),
+        .testTarget(name: "SwiftWASMWorkshopTests")
     ]
 )
---
+++ Sources/SwiftWASMWorkshop/SwiftWASMWorkshop.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class SwiftWASMWorkshop {
+    func hello() -> String {
+        return "Hello, SwiftWASMWorkshop!"
+    }
+}
